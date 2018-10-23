03a40b1e60f6bf1586841e0fd2c3aea0c9e62f91
sabio <armandojpr@yahoo.com>
2018-10-22 21:47:40 -0500

    Some refactoring done. I divided this project into two separated projects, a client and a server. I'm still working on it

---
+++ client/src/main/java/com/demos/demowebfluxclient/DemowebfluxClientApplication.java
@@ -0,0 +1,12 @@
+package com.demos.demowebfluxclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemowebfluxClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemowebfluxClientApplication.class, args);
+	}
+}
